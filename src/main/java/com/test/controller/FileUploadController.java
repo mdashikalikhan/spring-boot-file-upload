0ba76316fda9ec1c06ca216a771ca594b250743a
ashik.khan <ashik.khan@N1084.spftl.com>
2018-05-23 12:27:08 +0600

    Few corrections

--- src/main/java/com/test/controller/FileUploadController.java
+++ src/main/java/com/test/controller/FileUploadController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class FileUploadController {
 
 	//destination folder to upload the files
-	private static String UPLOAD_FOLDER = "\\\\file02\\rnd\\Abdullah\\";
+	private static String UPLOAD_FOLDER = "D:\\temp\\";//"\\\\file02\\rnd\\Abdullah\\";
 
 	@RequestMapping("/upload")
 	public ModelAndView showUpload() {
